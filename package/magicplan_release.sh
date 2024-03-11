9ba20ead64ca0888657aa91b11018e2f8c37571f
Loic Dal Zotto <loic.dalzotto@magicplan.app>
2024-03-11 17:28:26 +0100

    Commit a script file to release

---
+++ package/magicplan_release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eu
+
+script_dir="$(cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+parent_dir="$(dirname "$script_dir")"
+
+# Go to script dir
+cd "$script_dir"
+yarn
+yarn build
+
+# Remove all files in parent dir except for package folder and gitignore
+cd "$parent_dir"
+rm -r ./docs
+rm -r ./CODE_OF_CONDUCT.md
+rm -r ./CONTRIBUTING.md
+rm -r ./tea.yaml
+
+# Copy all files from package to parent dir
+cd "$script_dir"
+rm -r ./scripts
+rm -r ./example
+rm -r ./yarn.lock
+rm -r ./tsconfig.json
+cp -r ./* ../
+
+
+cd "$parent_dir"
+rm -r ./package
+rm -r .gitignore
+rm -r .github
+rm -r ./node_modules
+
+git add .
+git commit -m "Release"
+
+rm -r ./magicplan_release.sh
